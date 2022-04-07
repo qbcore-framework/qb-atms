63b3432dbc5baefa05b7b3d43a3236874c7ee3b3
Matt <matthewrorke@gmail.com>
2022-04-07 17:53:29 +0200

    Add ATMModel objects as Literals

--- config.lua
+++ config.lua
@@ -3,8 +3,8 @@ Config = {}
 Config.DailyLimit = 5000
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
 Config.ATMModels = {
-    "prop_atm_01",
-    "prop_atm_02",
-    "prop_atm_03",
-    "prop_fleeca_atm"
+    `prop_atm_01`,
+    `prop_atm_02`,
+    `prop_atm_03`,
+    `prop_fleeca_atm`
 }
